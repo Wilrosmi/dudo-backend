3faf1edb4dab967e1bc2524d8ab193d360029383
Wilrosmi <will.r.smith31@gmail.com>
2022-09-22 11:38:00 +0100

    Initial setup

--- src/index.ts
+++ src/index.ts
@@ -15,50 +15,18 @@ const io = new Server(server, {
     },
 });
 
-const storyWords: string[] = [];
-
-io.on("connection", (s: Socket) => {
+io.on("connection", (socket: Socket) => {
     console.log("Got a connection.  Registering handlers...");
 
     //useful for debugging
-    s.onAny((tag: string, ...otherArgs) =>
+    socket.onAny((tag: string, ...otherArgs) =>
         console.log("Received: ", tag, otherArgs, "(generic handler)")
     );
 
-    //register to handle specific messages...
-    s.on("addWord", (newWord: string) => {
-        storyWords.push(newWord);
-        //tell everyone, including socket s
-        io.emit("storyUpdate", storyWords);
-    });
-
-    s.on("clearStory", (newWord: string) => {
-        storyWords.length = 0; //reset array to [], even though const.
-        //Tell everyone, including socket s
-        io.emit("storyUpdate", storyWords);
-    });
-
-    s.on("sayToOthers", (msg: string) => {
-        //Sends to all connected, EXCEPT not to this socket, s
-        s.broadcast.emit("chat", { from: s.id, msg: msg });
-    });
-});
-
-//Send something every 20 seconds to all connected
-setInterval(() => {
-    io.emit("time", new Date());
-}, 20000);
-
-//express routes - optional
-app.get("/", (req, res) => {
-    res.send("try POSTing {word: 'blah'} to /storyWords");
-});
-
-app.post("/storyWords", (req, res) => {
-    const word = req.body.word;
-    storyWords.push(word);
-    res.status(201).json({ outcome: "success", storyWords: storyWords });
-    io.emit("storyUpdate", storyWords);
+    // socket.on("sayToOthers", (msg: string) => {
+    //     //Sends to all connected, EXCEPT not to this socket, s
+    //     socket.broadcast.emit("chat", { from: socket.id, msg: msg });
+    // });
 });
 
 const port = process.env.PORT ?? 4000;
